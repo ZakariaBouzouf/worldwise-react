67fba369653d7884446fa66bc85838461f0d982f
dezo <dezo@localhost.localdomain>
2024-04-19 01:06:10 +0200

    Refactoring using the Context API

--- src/components/CountryList.jsx
+++ src/components/CountryList.jsx
@@ -2,8 +2,10 @@ import styles from './CountryList.module.css'
 import Spinner from './Spinner'
 import Message from './Message'
 import CountryItem from './CountryItem'
+import { useCities } from '../contexts/CitiesContext'
 
-export default function CountryList({ cities, isLoading }) {
+export default function CountryList() {
+  const { cities, isLoading } = useCities()
   if (isLoading) return <Spinner />
 
   if (!cities.length) return <Message message="Add your city by clicking on the map." />
@@ -12,10 +14,11 @@ export default function CountryList({ cities, isLoading }) {
     if (!arr.map(el => el.city).includes(city.country)) {
       return [...arr, { country: city.country, emoji: city.emoji }]
     } else return arr;
-  },[]);
+  }, []);
+  console.log(countries)
 
 
   return (
-    <ul className={styles.countryList}>{countries.map(country => <CountryItem country={country} key={country.id} />)}</ul>
+    <ul className={styles.countryList}>{countries.map(country => <CountryItem country={country} key={country.country} />)}</ul>
   )
 }
